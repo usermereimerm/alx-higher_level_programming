3dc6ebf230ab15bcd95ab6cb50683b662a61cce6
usama <uauwal941@gmail.com>
2022-11-30 20:47:36 +0000

    correction of first python task

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:65] + str[-22:-19] + str[0:5]
+str = str[39:66] + str[106:112] + str[:6]
 print(str)
